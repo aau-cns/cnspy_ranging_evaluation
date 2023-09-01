210a77609e2bfba0f8a9e2a325c37cd498a3ec7d
Roland Jung <rolandjung0@gmail.com>
2023-09-01 15:09:26 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Get the release/version string
+with open(path.join(here, 'RELEASE'), encoding='utf-8') as f:
+    release = f.read()
+
+setup(
+    name='cnspy_trajectory_evaluation',
+    version=release,
+    author='Roland Jung',
+    author_email='roland.jung@aau.at',    
+    description='Evaluation of range measurements.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/aau-cns/cnspy_ranging_evaluation/',
+    project_urls={
+        "Bug Tracker": "https://github.com/aau-cns/cnspy_ranging_evaluation/issues",
+    },    
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    
+    packages=find_packages(exclude=["test_*", "TODO*"]),
+    python_requires='>=3.6',
+    install_requires=['numpy', 'pandas', 'spatialmath-python', 'scipy', 'matplotlib', 'joblib', 'configparser', 'cnspy_numpy_utils', 'cnspy_timestamp_association' ],
+)
